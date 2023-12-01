38f82cb43a006082d5ec02bdd2084e47980b8b14
LorenzoTausani <83968253+LorenzoTausani@users.noreply.github.com>
2023-12-01 16:57:43 +0100

    Delete local_settings.example.py

--- xdream/net_utils/local_settings.example.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-# whether gpu is available for NN computation
-gpu_available = True
-# root directory where network defs and weights are stored; see net_catalogue.py
-nets_dir = Path('~/Documents/nets').expanduser()
